c183da6b9958f51b39ee9dbbab7819ffd60a95c0
Clay Danford <clay_danford@neimanmarcus.com>
2020-08-03 16:42:47 -0500

    Add initial files.

---
+++ src/aws/name.ts
@@ -0,0 +1,46 @@
+import md5 from 'md5'
+import _ from 'lodash'
+import * as util from 'util'
+
+const TEXT = {
+  POLICYSCALE: 'AutoScalingPolicy-%s',
+  TARGET: 'AutoScalingTarget-%s',
+}
+
+const clean = (input: string): string =>
+  truncate(input.replace(/[^a-z0-9+]+/gi, ''))
+
+const truncate = (input: string): string =>
+  input.length <= 64 ? input : input.substr(0, 32) + md5(input)
+
+const ucfirst = (data: string): string => `${_.upperFirst(data)}`
+
+export default class Name {
+  constructor(private options: Options) {}
+
+  public target(func: string): string {
+    return clean(this.build(TEXT.TARGET, func))
+  }
+
+  public policy(func: string): string {
+    return clean(this.build(TEXT.POLICYSCALE, func))
+  }
+
+  public PCAliasLogicalId(functionName: any) {
+    return `${this.normalizedFunctionName(functionName)}ProvConcLambdaAlias`
+  }
+
+  private normalizedFunctionName(functionName: string) {
+    return ucfirst(
+      functionName.replace(/-/g, 'Dash').replace(/_/g, 'Underscore')
+    )
+  }
+
+  private build(data: string, ...args: string[]): string {
+    return [args ? util.format(data, ...args) : data, this.suffix()].join('')
+  }
+
+  private suffix(): string {
+    return [this.options.stage, this.options.region].map(ucfirst).join('')
+  }
+}
---
+++ src/aws/target.ts
@@ -0,0 +1,37 @@
+import Resource from './resource'
+
+export default class Target extends Resource {
+  private readonly type = 'AWS::ApplicationAutoScaling::ScalableTarget'
+
+  private readonly roleArn =
+    'arn:aws:iam::${AWS::AccountId}:role/aws-service-role/lambda.application-autoscaling.amazonaws.com/AWSServiceRoleForApplicationAutoScaling_LambdaConcurrency'
+
+  constructor(options: Options, private data: Configuration) {
+    super(options)
+  }
+
+  public toJSON(): any {
+    const resource = `function:${this.options.service}-${this.options.stage}-${this.data.function}:provisioned`
+
+    const nameTarget = this.name.target(this.data.function)
+
+    const DependsOn = [this.name.PCAliasLogicalId(this.data.function)].concat(
+      this.dependencies
+    )
+
+    return {
+      [nameTarget]: {
+        DependsOn,
+        Properties: {
+          MaxCapacity: this.data.maximum,
+          MinCapacity: this.data.minimum,
+          ResourceId: resource,
+          RoleARN: { 'Fn::Sub': this.roleArn },
+          ScalableDimension: 'lambda:function:ProvisionedConcurrency',
+          ServiceNamespace: 'lambda',
+        },
+        Type: this.type,
+      },
+    }
+  }
+}
