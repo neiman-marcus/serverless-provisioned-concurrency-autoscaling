5e91208bccab9a26d2f2b2ce1cf8fcff76da2002
Chad Berchek <chadberchek@carfax.com>
2021-10-27 14:40:26 -0500

    Remove md5 package dependency
    Eliminates 4 transitive dependencies for users

--- src/utility.ts
+++ src/utility.ts
@@ -1,8 +1,12 @@
-import md5 from 'md5'
+import { createHash } from 'crypto'
 import _ from 'lodash'
 
 export const clean = (input: string): string => {
   return truncate(input.replace(/[^a-z0-9+]+/gi, ''))
+}
+
+const md5 = (input: string): string => {
+  return createHash("md5").update(input).digest("hex")
 }
 
 export const truncate = (input: string): string => {
